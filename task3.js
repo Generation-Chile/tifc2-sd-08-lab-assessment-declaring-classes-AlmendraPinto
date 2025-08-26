382e948a36d12c8b86dcb6667303597373f4c202
AlmendraPinto <almendra.pinto@genstudents.org>
2025-08-26 14:23:57 +0000

    alme lab

--- task3.js
+++ task3.js
@@ -1,8 +1,11 @@
 export class Player {
-  constructor() {
-  
-  }
-
-
-
+  constructor(name,level) {
+   this.name = name;
+   this.level = level;
+ }
+ info() {
+   console.log(`${this.name} has reached Level ${this.level}!`);
+ }
 }
+const player = new Player("Tara", 6);
+player.info();
